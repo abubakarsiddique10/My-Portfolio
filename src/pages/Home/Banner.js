ddd00dab1935f3ba23f02ca097c40924c908d29e
abubakarsiddique10 <abubakarsiddique7912@gmail.com>
2022-07-20 13:10:58 +0600

    change img

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
         <section id="banner" className="bg-white">
             <div className="container bg-[url(https://i.ibb.co/mvYYy9d/Main-menu-1.png)]" style={{ backgroundPosition: "68% 49%", backgroundRepeat: "no-repeat" }}>
                 <div className="hero-content flex-col lg:flex-row-reverse lg:h-[650px] py-10 lg:p-0 gap-8 lg:gap-32">
-                    <img src="https://i.ibb.co/7CX6HCV/Untitled-design-2-removebg-preview-1.png" className="max-w-sm baaner-img w-full bg-[#FBFBFB]" />
+                    <img src="https://i.ibb.co/pxNnzN3/abu-bakaar-1.png" className="max-w-sm baaner-img w-full bg-[#FBFBFB]" />
                     <div className="text-center sm:text-left ">
                         <h5 className="font-medium mb-1 text-xl">Hi, my neme is</h5>
                         <h1 className="text-3xl sm:text-5xl font-bold banner-title">Abu Bakar and I'm a Front-end Developer </h1>

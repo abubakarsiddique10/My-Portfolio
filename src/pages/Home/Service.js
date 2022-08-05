04f882e5dfb0e77c7bd824a0934c9a0ef66e3f1b
abubakarsiddique10 <abubakarsiddique7912@gmail.com>
2022-08-05 23:48:12 +0600

    update

--- src/pages/Home/Service.js
+++ src/pages/Home/Service.js
@@ -2,7 +2,7 @@ import React from "react";
 const Service = ({ service }) => {
     const { icon, title, description } = service;
     return (
-        <div className="bg-base-100 flex flex-col items-center text-center p-10 hover:shadow-lg transition duration-300 ease-in-out text-black">
+        <div className="bg-base-100 flex flex-col items-center text-center p-10 hover:shadow-lg transition duration-300 ease-in-out">
             <figure><img src={icon} alt="Shoes" className="" /></figure>
             <div className="">
                 <h2 className="text-2xl font-medium mt-5 mb-2">{title}</h2>

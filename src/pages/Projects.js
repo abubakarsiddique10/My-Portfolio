0eeae0aef17587efffed51095bd179c7d44d91c4
abubakarsiddique10 <abubakarsiddique7912@gmail.com>
2022-10-03 15:25:57 +0600

    update portfolio

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,14 +4,15 @@ import Project from "./Project";
 
 const Projects = () => {
     const [projects] = useData();
-
+    const newProjects = projects.slice(0, 3);
+    console.log(newProjects)
     return (
         <section id="portfolio" className="">
             <div className="container">
                 <h5 className="text-4xl font-medium text-center mb-20 title">My portfolio</h5>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
                     {
-                        projects.map(project => <Project project={project} key={project.id} />)
+                        newProjects.map(project => <Project project={project} key={project.id} />)
                     }
                 </div>
             </div>
@@ -20,4 +21,4 @@ const Projects = () => {
 }
 export default Projects;
 
-/* https://hidden-sierra-55763.herokuapp.com/projectsá */	
+/* https://hidden-sierra-55763.herokuapp.com/projectsá */
